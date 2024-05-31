86bf50c1f3e1b9026c986ec814f5c2a5e20947bc
Tyler127 <2002tjlarson2@gmail.com>
2024-05-31 13:43:36 -0500

    i sure love nextjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<h1>test does this show up </h1></body>
     </html>
   );
 }
